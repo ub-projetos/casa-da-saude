e6d7584cd62bee11e224b5cfd2525b92f911ff95
thayna-bezerra <arraisthayna2106@gmail.com>
2024-05-14 12:18:50 -0300

    Criação de classe utilitária

---
+++ app/lib/util/AppHelper.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * AppHelper
+ *
+ * @package    model
+ * @subpackage util
+ * @author     Thayna Bezerra
+ */
+class AppHelper
+{
+    public static function lista_ativo()
+    {
+        return [
+            'Y' => 'Sim',
+            'N' => 'Não'
+        ];
+    }
+}
